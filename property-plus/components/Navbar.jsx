d35ca0c8cd401518842922749b2f23bd3d7b16e7
jonhealy1 <jonathan.d.healy@gmail.com>
2025-02-05 12:53:25 +0800

    update html names

--- property-plus/components/Navbar.jsx
+++ property-plus/components/Navbar.jsx
@@ -18,13 +18,13 @@ const Navbar = () => {
                 className="block h-6 w-6"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 aria-hidden="true"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                 />
               </svg>
@@ -92,13 +92,13 @@ const Navbar = () => {
                   className="h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   aria-hidden="true"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
                   />
                 </svg>
@@ -135,13 +135,13 @@ const Navbar = () => {
                 role="menu"
                 aria-orientation="vertical"
                 aria-labelledby="user-menu-button"
-                tabindex="-1"
+                tabIndex="-1"
               >
                 <a
                   href="/profile.html"
                   className="block px-4 py-2 text-sm text-gray-700"
                   role="menuitem"
-                  tabindex="-1"
+                  tabIndex="-1"
                   id="user-menu-item-0"
                 >
                   Your Profile
@@ -150,7 +150,7 @@ const Navbar = () => {
                   href="/saved-properties.html"
                   className="block px-4 py-2 text-sm text-gray-700"
                   role="menuitem"
-                  tabindex="-1"
+                  tabIndex="-1"
                   id="user-menu-item-2"
                 >
                   Saved Properties
@@ -158,7 +158,7 @@ const Navbar = () => {
                 <button
                   className="block px-4 py-2 text-sm text-gray-700"
                   role="menuitem"
-                  tabindex="-1"
+                  tabIndex="-1"
                   id="user-menu-item-2"
                 >
                   Sign Out
