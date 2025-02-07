64a26ed308c9f0be11f2264598d1d3c6bef89308
jonhealy1 <jonathan.d.healy@gmail.com>
2025-02-07 13:48:22 +0800

    hero, components

--- property-plus/app/page.jsx
+++ property-plus/app/page.jsx
@@ -1,11 +1,12 @@
-import Link from "next/link";
+import Hero from "../components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Welcome</h1>
-      <Link href="/properties">Properties</Link>
-    </div>
+    <>
+      <Hero />
+      <InfoBoxes />
+    </>
   );
 };
 
---
+++ property-plus/components/InfoBox.jsx
@@ -0,0 +1,10 @@
+const InfoBox = () => {
+  return (
+    <div className="info-box">
+      <h1>Property Plus</h1>
+      <p>Find your dream home today</p>
+    </div>
+  );
+};
+
+export default InfoBox;
