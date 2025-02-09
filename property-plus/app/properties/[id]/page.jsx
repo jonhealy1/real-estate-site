f00dec2f7648757be95400e74986045951cf79cd
jonhealy1 <jonathan.d.healy@gmail.com>
2025-02-10 00:05:21 +0800

    single property page header image

--- property-plus/app/properties/[id]/page.jsx
+++ property-plus/app/properties/[id]/page.jsx
@@ -1,8 +1,27 @@
-const PropertyPage = ({ params }) => {
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
+
+const PropertyPage = async ({ params }) => {
+  await connectDB();
+  const property = await Property.findById(params.id).lean();
   return (
-    <div>
-      <h1>Property Page {params.id}</h1>
-    </div>
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" /> Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section>{property.name}</section>
+    </>
   );
 };
 
