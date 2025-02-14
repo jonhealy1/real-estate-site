ac4cd8159c40f52ad8154dc8dd372905f9ff3d4a
jonhealy1 <jonathan.d.healy@gmail.com>
2025-02-14 14:38:58 +0800

    property edit form

--- property-plus/app/layout.jsx
+++ property-plus/app/layout.jsx
@@ -1,26 +1,32 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import "@/assets/styles/globals.css";
 import AuthProvider from "@/components/AuthProvider";
+import { ToastContainer } from "react-toastify";
+import { GlobalProvider } from "@/context/GlobalContext";
+import "@/assets/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
+import "photoswipe/dist/photoswipe.css";
 
 export const metadata = {
-  title: "Property Plus",
-  keywords: "real estate, property, rental",
-  description: "Real estate and property rental",
+  title: "PropertyPulse | Find The Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
 };
 
 const MainLayout = ({ children }) => {
   return (
-    <AuthProvider>
-    <html>
-      <body className="font-sans">
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
-    </AuthProvider>
+    <GlobalProvider>
+      <AuthProvider>
+        <html lang="en">
+          <body>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </AuthProvider>
+    </GlobalProvider>
   );
 };
-
 export default MainLayout;
