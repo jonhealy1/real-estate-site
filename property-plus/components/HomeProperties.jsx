b1948160954d724bae3f920743b4da531f115934
jonhealy1 <jonathan.d.healy@gmail.com>
2025-02-14 13:05:50 +0800

    add components

--- property-plus/components/HomeProperties.jsx
+++ property-plus/components/HomeProperties.jsx
@@ -1,36 +1,37 @@
-import PropertyCard from "./PropertyCard";
-import connectDB from "@/config/database";
-import Property from "@/models/Property";
 import Link from "next/link";
+import PropertyCard from "@/components/PropertyCard";
+import { fetchProperties } from "@/utils/requests";
 
 const HomeProperties = async () => {
-  await connectDB();
-  const properties = await Property.find({})
-    .sort({ createdAt: -1 })
-    .limit(3)
-    .lean();
+  const data = await fetchProperties();
+
+  const recentProperties = data.properties
+    .sort(() => Math.random() - Math.random())
+    .slice(0, 3);
+
   return (
     <>
       <section className="px-4 py-6">
-        <div className="container-xl lg:container m-auto px-4 py-6">
+        <div className="container-xl lg:container m-auto">
           <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
             Recent Properties
           </h2>
-          {properties.length === 0 ? (
-            <p>No properties found</p>
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {properties.map((property) => (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {recentProperties.length === 0 ? (
+              <p>No Properties Found</p>
+            ) : (
+              recentProperties.map((property) => (
                 <PropertyCard key={property._id} property={property} />
-              ))}
-            </div>
-          )}
+              ))
+            )}
+          </div>
         </div>
       </section>
-      <section className="m-auto max-w-lg my-6 px-6">
+
+      <section className="m-auto max-w-lg my-10 px-6">
         <Link
           href="/properties"
-          className="block bg-black hover:bg-gray-700 text-white px-6 py-4 rounded-xl text-center"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
         >
           View All Properties
         </Link>
@@ -38,5 +39,4 @@ const HomeProperties = async () => {
     </>
   );
 };
-
 export default HomeProperties;
